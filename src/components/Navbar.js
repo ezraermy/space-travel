a43852ad68ab2ceddc0c27c9e9bfb04ada89ee41
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-04-18 20:25:53 +0430

    Update Navbar.jsx add styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,32 @@
-import { NavLink } from 'react-router-dom';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from 'assets/planet.png';
 
 const Navbar = () => {
   const links = [
-    { path: 'profile', text: 'Profile' },
-    { path: 'missions', text: 'Missions' },
     { path: '/', text: 'Rockets' },
+    { path: 'missions', text: 'Missions' },
+    { path: 'profile', text: 'My Profile' },
   ];
   return (
-    <div className="navbar">
-      <h1 className="nav-title">Space Travelers</h1>
-      <ul>
+    <div className="navbar flex justify-between items-center mx-12 py-4 border-b">
+      <h1 className="nav-title flex items-center text-3xl font-bold text-cyan-500 pl-6">
+        <img className="mr-4" src={Logo} width={48} height={48} />
+        Space Travelers Hub
+      </h1>
+      <ul className="flex justify-center">
         {links.map((link) => (
           <li key={link.text}>
-            <NavLink to={link.path}>{link.text}</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `text-sky-600 font-semibold px-6 ${isActive && 'underline'} ${
+                  link.path === 'profile' && 'border-l-2'
+                }`
+              }
+              to={link.path}
+            >
+              {link.text}
+            </NavLink>
           </li>
         ))}
       </ul>
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Rockets = () => (
-  <div>Rockets</div>
-);
+const Rockets = () => <div className="text-3xl text-blue-500">Rockets</div>;
 
 export default Rockets;
