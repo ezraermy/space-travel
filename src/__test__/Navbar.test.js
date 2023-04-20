dca6707ef644aafee452e8b869a9f5d27b38fa73
Ezra <ezraermy@gmail.com>
2023-04-20 14:48:40 +0300

    Merge test folder

--- src/__test__/Navbar.test.js
+++ src/__test__/Navbar.test.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 import Navbar from 'components/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
-describe('Navbar', () => {
-  test('renders the logo and links', () => {
-    const { getByText, getByAltText } = render(
-      <MemoryRouter>
-        <Navbar />
-      </MemoryRouter>,
-    );
+describe('Navbar component tests', () => {
+  test('Navbar should be rendered correctly', () => {
+    render(<Navbar />, { wrapper: BrowserRouter });
+    expect(screen.getByText('Space Travelers Hub')).toBeInTheDocument();
+  });
 
-    const logo = getByAltText(/logo/);
-    expect(logo).toBeInTheDocument();
-
-    const rocketsLink = getByText(/rockets/i);
-    expect(rocketsLink).toBeInTheDocument();
-
-    const missionsLink = getByText(/missions/i);
-    expect(missionsLink).toBeInTheDocument();
-
-    const profileLink = getByText(/my profile/i);
-    expect(profileLink).toBeInTheDocument();
+  test('Navbar should contain all 3 links', () => {
+    render(<Navbar />, { wrapper: BrowserRouter });
+    expect(screen.getByText('Rockets')).toBeInTheDocument();
+    expect(screen.getByText('Missions')).toBeInTheDocument();
+    expect(screen.getByText('My Profile')).toBeInTheDocument();
   });
 });
